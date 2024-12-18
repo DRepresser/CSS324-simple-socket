2c7dfed6c67b948606d43a20b165cead170fd70c
DRepresser <intgooguy@gmail.com>
2024-12-19 02:53:35 +0700

    Update docker-compose adding stdin_open and tty true

--- client/client.py
+++ client/client.py
@@ -5,8 +5,12 @@ PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    message = input("Enter message to send: ")
-    s.sendall(message.encode())
-    data = s.recv(1024)
-
-print(f"Received: {data.decode()}")
+    print("Connected to the server. Type your message (or 'exit' to quit):")
+    while True:
+        message = input("> ")
+        if message.lower() == 'exit':
+            print("Exiting.")
+            break
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print(f"Received: {data.decode()}")
